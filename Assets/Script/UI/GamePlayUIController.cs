bc79fee149018714e8dfc54f5aae142d8893f067
Fox <fook1222@gmail.com>
2024-10-26 09:36:55 +0700

    -

--- Assets/Script/UI/GamePlayUIController.cs
+++ Assets/Script/UI/GamePlayUIController.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 
 public class GamePlayUIController : MonoBehaviour
